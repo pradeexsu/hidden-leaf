f16274ba67cff02c55f02d92a576043a1ee2b435
Pradeep Suthar <pradeexsu@gmail.com>
2023-12-13 15:05:09 +0530

    css fix

--- src/components/TimeInput.tsx
+++ src/components/TimeInput.tsx
@@ -9,16 +9,17 @@ export default function TimeInput({
 }: TimeInputProps) {
   return (
     <>
-      <div className="flex justify-around ">
-        <label className="text-gray-700 " htmlFor="time">
+      <div className="flex justify-center gap-5 mb-20">
+        <label className="text-gray-700" htmlFor="time">
+          Start Time:
           <input
             type="time"
             className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
             onChange={(e) => setStartTime(e.target.value)}
           />
         </label>
-
         <label className="text-gray-700 " htmlFor="time">
+          End Time:
           <input
             type="time"
             className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
